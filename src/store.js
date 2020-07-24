7ea4f2d569ffb641c1ae8c1374cce1bc12dd7f6e
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-07-24 17:07:54 -0500

    Se hace uso del proyecto que sube el profe

--- src/store.js
+++ src/store.js
@@ -5,11 +5,8 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: null,
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  mutations: {},
+  actions: {},
 });
